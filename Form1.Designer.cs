69cfb1b54686aa491a5c7bb634412b76869b2159
Oséias <149209851+JucaClan@users.noreply.github.com>
2023-10-30 20:21:23 -0300

    Projeto finalizado
    Calculadora de IMC finalizada.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblIMC = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(30, 98);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(41, 16);
+            this.lblAltura.TabIndex = 0;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(30, 69);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(39, 16);
+            this.lblPeso.TabIndex = 2;
+            this.lblPeso.Text = "Peso";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(43, 21);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(161, 27);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "Calcule seu IMC:";
+            this.lblTitulo.UseCompatibleTextRendering = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalcular.AutoSize = true;
+            this.btnCalcular.BackColor = System.Drawing.Color.Orange;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(67, 150);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(137, 43);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(80, 65);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(103, 20);
+            this.txtPeso.TabIndex = 5;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(81, 97);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(101, 20);
+            this.txtAltura.TabIndex = 6;
+            // 
+            // lblIMC
+            // 
+            this.lblIMC.AutoSize = true;
+            this.lblIMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIMC.Location = new System.Drawing.Point(33, 211);
+            this.lblIMC.Name = "lblIMC";
+            this.lblIMC.Size = new System.Drawing.Size(38, 20);
+            this.lblIMC.TabIndex = 7;
+            this.lblIMC.Text = "IMC: ";
+            this.lblIMC.UseCompatibleTextRendering = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(6, 249);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 8;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.txtResultado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtResultado.Location = new System.Drawing.Point(81, 210);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(108, 13);
+            this.txtResultado.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(247, 280);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblIMC);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblAltura);
+            this.Name = "Form1";
+            this.Text = "Calculadora de IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblIMC;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtResultado;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,71 @@ namespace CalculadoraIMC
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Impedir que o usuária quebre o código usando letras
+            try
+            {
+                //obter números do form
+                double altura, peso, calculo;
+                //variaveis recebem txt
+                altura = double.Parse(txtAltura.Text);
+                peso = double.Parse(txtPeso.Text);
+                //calcular IMC
+                calculo = peso / (altura * altura);
+
+                //verificar o IMC do usuário e mostrar sua classificação
+                if (calculo <= 18.5)
+                {
+                    txtResultado.Text = Math.Round(calculo, 3).ToString();
+                    lblStatus.ForeColor = Color.Red;
+                    lblStatus.Text = "Você está baixo do peso!";
+
+                }
+                else if (calculo >= 18.6 && calculo <= 24.9)
+                {
+                    txtResultado.Text = Math.Round(calculo, 3).ToString();
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Text = "Você está no peso ideal, Parabéns!";
+                }
+                else if (calculo >= 25.0 && calculo <= 29.9)
+                {
+                    txtResultado.Text = Math.Round(calculo, 3).ToString();
+                    lblStatus.ForeColor = Color.OrangeRed;
+                    lblStatus.Text = "Você está levemente acima do peso!";
+                }
+                else if (calculo >= 30.0 && calculo <= 34.9)
+                {
+                    txtResultado.Text = Math.Round(calculo, 3).ToString();
+                    lblStatus.ForeColor = Color.Red;
+                    lblStatus.Text = "Você está com obesidade grau I!";
+                }
+                else if (calculo >= 35.0 && calculo <= 39.9)
+                {
+                    txtResultado.Text = Math.Round(calculo, 3).ToString();
+                    lblStatus.ForeColor = Color.Red;
+                    lblStatus.Text = "Você está com obesidade grau II, Severa!";
+                }
+                else
+                {
+                    txtResultado.Text = Math.Round(calculo, 3).ToString();
+                    lblStatus.ForeColor = Color.Red;
+                    lblStatus.Text = "Você está com obesidade grau III, Morbida!";
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Dados informados inválidos! ");
+                //limpar os txts
+                txtAltura.Clear();
+                txtPeso.Clear();
+                txtResultado.Clear();
+                lblStatus.Text = "";
+                
+            }
+
+
+        }   
     }
 }
